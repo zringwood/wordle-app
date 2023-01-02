597f7e05fae66c385781dcc738d27416fc41faf2
Zachary Ringwood <zringwood@gmail.com>
2023-01-02 04:11:47 -0500

    Add files via upload
    Added a button.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-from wtforms import Form, StringField, validators
-import json, os
+from wtforms import Form, StringField, SubmitField, validators
+import json
 from WordleGuessesGenerator import Puzzle
 app = Flask(__name__)
 
 class WordleValidation(Form):
     answer = StringField('Puzzle Answer',[validators.Length(min=5,max=5)])
+    button = SubmitField('Submit',[])
 
 @app.route('/', methods = ['GET', 'POST'])
 def processinput():
@@ -23,6 +24,4 @@ def processinput():
                 guesses[i] = nxt
     return render_template('inputpage.html', output=guesses, form=form)
 
-if __name__ == '__main__':
-    server_port = os.environ.get('PORT', '8080')
-    app.run(debug=False, port=server_port, host='0.0.0.0')
+app.run()
